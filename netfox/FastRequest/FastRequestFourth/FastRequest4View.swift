1f39bf8bf470813968e11f879849c9f0e101ae80
user1000 <user1000@unlabeled.qupra.nl>
2025-04-04 21:17:27 +0200

    fix 14

--- netfox/FastRequest/FastRequestFourth/FastRequest4View.swift
+++ netfox/FastRequest/FastRequestFourth/FastRequest4View.swift
@@ -82,11 +82,12 @@ public struct FastRequest4View: View {
                                 .navigationBarBackButtonHidden(true)
                         }
                     }
-                    .fullScreenCover(isPresented: $showIntermediateScreen) {
+                    .navigationDestination(isPresented: $showIntermediateScreen) {
                         if let obj = model?.gap?.objecs[(model?.gap?.orderIndex ?? 1) - 1] {
                             InterScreen(showNextScreen: .constant(false), isDisabled: $isDisabled, model: model, currentTariff: currentTariff, scanObject: obj, scanTitle: model?.gap?.title ?? "", secureScreenNumber: model?.gap?.orderIndex ?? 0,
                                         rScreen: self.rScreen ?? 0,
                                         completion: completion)
+                            .navigationBarBackButtonHidden(true)
                         }
                     }
                     .onAppear {
