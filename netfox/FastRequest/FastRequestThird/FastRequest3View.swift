0e5a3265c3b8095f728018a53bc7515752aeae67
user1000 <user1000@unlabeled.qupra.nl>
2025-04-11 17:57:01 +0200

    new fix 7

--- netfox/FastRequest/FastRequestThird/FastRequest3View.swift
+++ netfox/FastRequest/FastRequestThird/FastRequest3View.swift
@@ -142,15 +142,19 @@ public struct FastRequest3View: View {
                         }
                     }
                     .navigationDestination(isPresented: $showIntermediateScreen) {
-                        if let obj = model?.gap?.objecs[(model?.gap?.orderIndex ?? 1) - 1] {
-                            let index = model?.gap?.orderIndex ?? 1
-                            if index != 0 {
-                                InterScreen(showNextScreen: .constant(false), isDisabled: $isDisabled, model: model, currentTariff: currentTariff, scanObject: obj, scanTitle: model?.gap?.title ?? "", secureScreenNumber: model?.gap?.orderIndex ?? 0,
-                                            rScreen: self.rScreen ?? 0,
-                                            completion: completion)
-                                .navigationBarBackButtonHidden(true)
+                        let index = model?.gap?.orderIndex ?? 1
+                        if index != 0 {
+                            if let obj = model?.gap?.objecs[(model?.gap?.orderIndex ?? 1) - 1] {
+                                let index = model?.gap?.orderIndex ?? 1
+                                if index != 0 {
+                                    InterScreen(showNextScreen: .constant(false), isDisabled: $isDisabled, model: model, currentTariff: currentTariff, scanObject: obj, scanTitle: model?.gap?.title ?? "", secureScreenNumber: model?.gap?.orderIndex ?? 0,
+                                                rScreen: self.rScreen ?? 0,
+                                                completion: completion)
+                                    .navigationBarBackButtonHidden(true)
+                                }
                             }
                         }
+                        
                     }
                     .onAppear {
                         completion(.specialOffer3Show)
